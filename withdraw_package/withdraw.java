c9d12c7e5eb7f3f187ca17dd33bcb5cfbde41a49
Preethz06 <126318224+Preethz06@users.noreply.github.com>
2023-10-01 13:58:18 +0530

    Update withdraw.java

--- withdraw_package/withdraw.java
+++ withdraw_package/withdraw.java
@@ -16,7 +16,7 @@ public class withdraw extends Component {
 
         String url="jdbc:mysql://127.0.0.1:3306";
         String uname="root";
-        String paswd="PRIPREETH26";
+        String paswd="0000";
         String databaseName="bank";
 
         JPanel upperpanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
